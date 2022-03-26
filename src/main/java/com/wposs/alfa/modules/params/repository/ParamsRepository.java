4aa9c25e8dd6651816044395848815599f91bc77
Jose Lara <joselara@wposs.com>
2022-03-26 18:08:50 -0300

    WP-R001-CONSTRUCCION-ARQUITECTURA
    * Creacion de controlador para el primer servicio para parametros de la app
    * Conexion a Oracle

--- src/main/java/com/wposs/alfa/modules/params/repository/ParamsRepository.java
+++ src/main/java/com/wposs/alfa/modules/params/repository/ParamsRepository.java
@@ -1,104 +1,77 @@
 package com.wposs.alfa.modules.params.repository;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.ListResourceBundle;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import com.wposs.core.jdbc.BaseResultSet;
+import com.wposs.alfa.modules.params.model.Business;
+import com.wposs.alfa.modules.params.model.Categories;
 import com.wposs.core.repository.BaseRepositoryDAO;
-import com.wposs.core.repository.ResultSetIterator;
-import org.springframework.jdbc.core.JdbcTemplate;
-import com.wposs.core.repository.Sql;
 import com.wposs.core.repository.Transaction;
 
 @Component
 public class ParamsRepository extends BaseRepositoryDAO{
 
-	String sql;
-	Map<String, Object> response = new HashMap<>();
-	List<String> lisResponse;
+
 	
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-	@Sql(	name="getParameters",
-			sql="select serial from equipos where id_equipo = ? "
-			)
-	public  Map<String, Object> getParameters(Transaction <?> t, Map<String, Object> request) throws Exception  {
+	public List<Categories> getCategories(Transaction <?> t, Map<String, Object> request) throws Exception  {
 		
-		Map<String, Object> responseDB = new HashMap<>();
-		
-		
-		
-		
-		
-		
-		sql = "select serial, modelo from equipos where estado = 1";
+		List<Categories> categories = new ArrayList<>();
+		String sql = "SELECT  "
+				+ "ID_CATEGORIE_BUSINESS,NAME, DESCRIPTION, URL_IMG "
+				+ "FROM DANKO.DANKO_CATEGORIES_BUSINESS "
+				+ "WHERE DANKO_STATE = ?";
+
+		List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, new Object[] {
+				1
+		});
+
+		if(rows != null) {
+			for (Map<String, Object> row : rows) {
+				Categories categorie = new Categories();
+				categorie.setIdCategorie((String) row.get("ID_CATEGORIE_BUSINESS"));
+				categorie.setName((String) row.get("NAME"));
+				categorie.setDescription((String) row.get("DESCRIPTION"));
+				categorie.setImg((String) row.get("URL_IMG"));
+				categories.add(categorie);
+			}
+		}
+		return categories;
+	}
 		
 
+	public List<Business> getBusiness(Transaction <?> t, Map<String, Object> request) throws Exception  {
 		
-		
-		/*List<Map<String, Object>> result =  jdbcTemplate.queryForList(sql, new Object[] {
-				request.get("id_equipo")
+		List<Business> business = new ArrayList<>();
+		String sql = "SELECT  "
+				+ "ID_BUSINESS,NAME, DESCRIPTION,IMG, URL,ID_CATEGORIE "
+				+ "FROM DANKO.DANKO_BUSINESS "
+				+ "WHERE DANKO_STATE = ?";
+
+		List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, new Object[] {
+				1
 		});
-		System.out.println("RESULTADO::"+result);
-		
-		
-		
-		for (Map<String, Object> map : result) {
-		    for (Map.Entry<String, Object> entry : map.entrySet()) {
-		        System.out.println(entry.getKey() + " - " + entry.getValue());
-		    }
+
+		if(rows != null) {
+			for (Map<String, Object> row : rows) {
+				Business bussn = new Business();
+				bussn.setIdCategorie((String) row.get("ID_BUSINESS"));
+				bussn.setName((String) row.get("NAME"));
+				bussn.setDescription((String) row.get("DESCRIPTION"));
+				bussn.setImg((String) row.get("IMG"));
+				bussn.setUrlBusiness((String) row.get("URL"));
+				bussn.setIdCategorie((String) row.get("ID_CATEGORIE"));
+				business.add(bussn);
+			}
 		}
-		
-		System.out.println("PASOOOOO");
-		
-		for (Map<String, Object> map : result) {
-		    for (Map.Entry<String, Object> entry : map.entrySet()) {
-		        String key = entry.getKey();
-		        Object value = entry.getValue();
-		    }
-		    System.out.println("PASE PRIMERO");
-		}
-		
-		
-		
-		result.forEach( rowMap -> {
-			responseDB.put("serial", (String) rowMap.get("serial"));
-			responseDB.put("modelo",  (String) rowMap.get("modelo"));
-		});*/
-		
-		System.out.println("Response::"+responseDB.toString());
-		
-		return responseDB;
-	}
-	
-	private void utilsBD () {
-		List<Map<String, Object>> result = null;
-		
-		for (Map<String, Object> map : result) {
-		    for (Map.Entry<String, Object> entry : map.entrySet()) {
-		        System.out.println(entry.getKey() + " - " + entry.getValue());
-		    }
-		}
-		
-		System.out.println("PASOOOOO");
-		
-		for (Map<String, Object> map : result) {
-		    for (Map.Entry<String, Object> entry : map.entrySet()) {
-		        String key = entry.getKey();
-		        Object value = entry.getValue();
-		    }
-		    System.out.println("PASE PRIMERO");
-		}
+		return business;
 	}
 
 }
--- src/main/java/com/wposs/alfa/modules/params/service/ParamsServices.java
+++ src/main/java/com/wposs/alfa/modules/params/service/ParamsServices.java
@@ -1,9 +1,14 @@
 package com.wposs.alfa.modules.params.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.wposs.alfa.modules.params.model.Business;
+import com.wposs.alfa.modules.params.model.Categories;
 import com.wposs.alfa.modules.params.repository.ParamsRepository;
 import com.wposs.core.repository.Transaction;
 import com.wposs.core.service.BaseSpringService;
@@ -14,7 +19,27 @@ public class ParamsServices extends BaseSpringService<ParamsRepository>{
 	public Map<String, Object> getParameters(Map<String, Object> request) throws Exception {
 		return beginReadTransaction(new Transaction<Map<String, Object>>() {
 			public Map<String, Object> doTransaction() throws Exception{
-				return getRepository().getParameters(this, request);
+				
+				Map<String, Object> response = new HashMap<>();
+				List<Categories> categoriesList = new ArrayList<>();
+				
+				List<Categories> categories = getRepository().getCategories(this, request);
+				
+				List<Business> business = getRepository().getBusiness(this, request);	
+				
+				
+				for(Categories categorie : categories) {
+					List<Business> businessLocal = new ArrayList<>();
+					for(Business bussn : business) {
+						if(bussn.getIdCategorie().equals(categorie.getIdCategorie())) {
+							businessLocal.add(bussn); 							
+						}
+					}
+					categorie.setBusiness(business);
+					categoriesList.add(categorie);
+				}
+				response.put("parameters", categoriesList);
+				return response;
 			}
 			
 		});
